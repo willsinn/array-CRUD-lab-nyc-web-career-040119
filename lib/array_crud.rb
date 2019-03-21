7037ddb12715ce84387f553b842930daaad5226c
w.h.sinn <w.h.sinn@gmail.com>
2019-03-21 09:40:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,11 +11,10 @@ end
 
 def add_element_to_start_of_array(array, element)
      array.unshift(element) 
-
 end
 
 def remove_element_from_end_of_array(array)
-  
+    array.pop  
 end
 
 def remove_element_from_start_of_array(array)
