cea3bac23370accc801ae2aae7e6ddb02228040b
w.h.sinn <w.h.sinn@gmail.com>
2019-03-21 09:34:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  an_array.push(element)
+  an_array.concat("element")
 end
 
 def add_element_to_start_of_array(array, element)
